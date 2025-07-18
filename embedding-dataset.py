593389d0f04ad1f937f4951ff5a0f2d4ab1cbcad
Jimboni-tech <yunchengzhou@gmail.com>
2025-07-17 21:28:29 -0500

    created embeddings dataset

---
+++ embedding-dataset.py
@@ -0,0 +1,16 @@
+#sentence-transformers/all-mpnet-base-v2
+#FAISS
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+from tqdm import tqdm  # Import tqdm for progress bar
+
+try:
+    sub_df = pd.read_csv("datasets/top-subreddits.csv")
+    sub_df['text'] = sub_df['name'] + ' ' + sub_df['public_description'].fillna('')
+    model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
+    embeddings = model.encode(sub_df['text'].tolist(), show_progress_bar=True)
+
+    sub_df['embeddings'] = list(embeddings)
+    sub_df.to_csv('datasets/top-subreddits-embeddings.csv')
+except Exception as e:
+    print('Error: ', e)
